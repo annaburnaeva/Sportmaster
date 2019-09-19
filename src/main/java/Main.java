1cb56f6cd12731d84a2947c355de391196aeb6ae
student <student@localhost>
2019-09-19 10:29:06 +0300

    Fixes #2 Added variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,24 @@ public class Main {
         int amountForBonuses = 1000;
         int bonus = 0;
 
+        int bonusesForStandart = 50;
+        int bonusesForSilver = 70;
+        int bonusesForGold = 100;
+
+        int standartTotalCost = 15000;
+        int goldTotalCost = 150000;
+
+
         int coefficientOfBonuses = presentCost / amountForBonuses;
 
-        if (totalCost <= 15000) {
-            bonus = 50 * coefficientOfBonuses;
+        if (totalCost <= standartTotalCost) {
+            bonus = bonusesForStandart * coefficientOfBonuses;
         }
-        if (totalCost > 15000 && totalCost <= 150000) {
-            bonus = 70 * coefficientOfBonuses;
+        if (totalCost > standartTotalCost && totalCost <= goldTotalCost) {
+            bonus = bonusesForSilver * coefficientOfBonuses;
         }
-        if (totalCost > 150000) {
-            bonus = 100 * coefficientOfBonuses;
+        if (totalCost > goldTotalCost) {
+            bonus = bonusesForGold * coefficientOfBonuses;
         }
 
         System.out.println(bonus);
